da846e944393756e5f0b5fdb09f108a8c41f97a2
Kostia <kostikas12@mail.ru>
2018-03-31 16:17:21 +0300

    add bootstrap

--- FrontPart/src/App.vue
+++ FrontPart/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand"><h4>Project</h4></router-link>
+      <form class="form-inline">
+        <router-link to="/registration" class="btn btn-secondary margin-right">Registration</router-link>
+        <router-link to="/login" class="btn btn-secondary">Login</router-link>
+      </form>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -11,4 +18,9 @@ export default {
 </script>
 
 <style>
+
+.margin-right {
+  margin-right:25px;
+}
+
 </style>
